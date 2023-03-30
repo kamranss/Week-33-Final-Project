a618d1db8a2ee7407d6afc6cbadad916067de770
kamranss <kamranss@code.edu.az>
2023-03-30 23:47:56 +0400

    Refactor: TAsk updated

---
+++ assets/script/index.js
@@ -0,0 +1,112 @@
+
+let modalContent = `<div class="select-location active" data-select2-id="3">
+<div class="select-location-wrapper">
+    <h6 class="entry-title">Choose your Delivery Location</h6>
+    <div class="entry-description">Enter your address and we will specify the offer for your area.</div>
+    <div class="close-popup"><i class="klbth-icon-x"></i></div><!-- close-popup -->
+    <div class="search-location">
+        <select size="8" name="site-area" class="site-area select2-hidden-accessible"
+            id="site-area" data-placeholder="Search your area" data-select2-id="site-area" tabindex="-1"
+            aria-hidden="true">
+            <option value="all" data-min="Clear All" data-select2-id="4">Select a Location</option>
+            <option value="alabama" data-min="Min: $130" data-select2-id="5">Alabama</option>
+            <option value="alaska" data-min="Min: $120" data-select2-id="6">Alaska</option>
+            <option value="arizona" data-min="Min: $150" data-select2-id="7">Arizona</option>
+            <option value="california" data-min="Min: $110" data-select2-id="8">California</option>
+            <option value="colorado" data-min="Min: $140" data-select2-id="9">Colorado</option>
+            <option value="florida" data-min="Min: $160" data-select2-id="10">Florida</option>
+            <option value="georgia" data-min="Min: $120" data-select2-id="11">Georgia</option>
+            <option value="kansas" data-min="Min: $170" data-select2-id="12">Kansas</option>
+            <option value="minnesota" data-min="Min: $120" data-select2-id="13">Minnesota</option>
+            <option value="new-york" data-min="Min: $110" data-select2-id="14">New York</option>
+            <option value="washington" data-min="Min: $130" data-select2-id="15">Washington</option>
+        </select><span
+            class="select2 select2-container select2-container--default select2-container--below select2-container--open"
+            dir="ltr" data-select2-id="1" style="width: 360px;"><span class="selection"><span
+                    class="select2-selection select2-selection--single" role="combobox" aria-haspopup="true"
+                    aria-expanded="true" tabindex="0" aria-disabled="false"
+                    aria-labelledby="select2-site-area-container" aria-owns="select2-site-area-results"
+                    aria-activedescendant="select2-site-area-result-9fbh-all"><span
+                        class="select2-selection__rendered" id="select2-site-area-container" role="textbox"
+                        aria-readonly="true"><span class="select2-selection__placeholder">Search your
+                            area</span></span><span class="select2-selection__arrow" role="presentation"><b
+                            role="presentation"></b></span></span></span><span class="dropdown-wrapper"
+                aria-hidden="true"></span></span><span
+            class="select2-container select2-container--default select2-container--open"
+            style="position: absolute; top: -79.12px; left: -588.6px;"><span
+                class="select2-dropdown site-location-select select2-dropdown--below" dir="ltr"
+                style="width: 360px;"><span class="select2-search select2-search--dropdown"><input
+                        class="select2-search__field" type="search" tabindex="0" autocomplete="off"
+                        autocorrect="off" autocapitalize="none" spellcheck="false" role="searchbox"
+                        aria-autocomplete="list" aria-controls="select2-site-area-results"
+                        placeholder="Search your area"
+                        aria-activedescendant="select2-site-area-result-9fbh-all"><svg
+                        data-testid="MagnifyGlass" viewBox="0 0 16 16" class="css-ekjjru">
+                        <path
+                            d="M14.23437,15.10449l-3.2539-3.25488a6.13908,6.13908,0,1,1,1.63574-1.85352c-.05859.10156-.11816.19922-.17969.29492l-.63086-.40625c.05567-.08593.10938-.17383.16016-.26269a5.406,5.406,0,1,0-1.19922,1.43945.37776.37776,0,0,1,.50781.02051l3.49121,3.49121Z"
+                            fill="currentColor"></path>
+                    </svg></span><span class="select2-results">
+                    <ul class="select2-results__options" role="listbox" id="select2-site-area-results"
+                        aria-expanded="true" aria-hidden="false">
+                        <li class="select2-results__option select2-results__option--highlighted"
+                            id="select2-site-area-result-9fbh-all" role="option" aria-selected="false"
+                            data-select2-id="select2-site-area-result-9fbh-all"><span>Select a
+                                Location</span><span class="min-price">Clear All</span></li>
+                        <li class="select2-results__option" id="select2-site-area-result-1mgl-alabama"
+                            role="option" aria-selected="false"
+                            data-select2-id="select2-site-area-result-1mgl-alabama"><span>Alabama</span><span
+                                class="min-price">Min: $130</span></li>
+                        <li class="select2-results__option" id="select2-site-area-result-nyqm-alaska"
+                            role="option" aria-selected="false"
+                            data-select2-id="select2-site-area-result-nyqm-alaska"><span>Alaska</span><span
+                                class="min-price">Min: $120</span></li>
+                        <li class="select2-results__option" id="select2-site-area-result-uggl-arizona"
+                            role="option" aria-selected="false"
+                            data-select2-id="select2-site-area-result-uggl-arizona"><span>Arizona</span><span
+                                class="min-price">Min: $150</span></li>
+                        <li class="select2-results__option" id="select2-site-area-result-044b-california"
+                            role="option" aria-selected="false"
+                            data-select2-id="select2-site-area-result-044b-california">
+                            <span>California</span><span class="min-price">Min: $110</span></li>
+                        <li class="select2-results__option" id="select2-site-area-result-dqaq-colorado"
+                            role="option" aria-selected="false"
+                            data-select2-id="select2-site-area-result-dqaq-colorado"><span>Colorado</span><span
+                                class="min-price">Min: $140</span></li>
+                        <li class="select2-results__option" id="select2-site-area-result-xxpu-florida"
+                            role="option" aria-selected="false"
+                            data-select2-id="select2-site-area-result-xxpu-florida"><span>Florida</span><span
+                                class="min-price">Min: $160</span></li>
+                        <li class="select2-results__option" id="select2-site-area-result-zc2v-georgia"
+                            role="option" aria-selected="false"
+                            data-select2-id="select2-site-area-result-zc2v-georgia"><span>Georgia</span><span
+                                class="min-price">Min: $120</span></li>
+                        <li class="select2-results__option" id="select2-site-area-result-9ox0-kansas"
+                            role="option" aria-selected="false"
+                            data-select2-id="select2-site-area-result-9ox0-kansas"><span>Kansas</span><span
+                                class="min-price">Min: $170</span></li>
+                        <li class="select2-results__option" id="select2-site-area-result-mv91-minnesota"
+                            role="option" aria-selected="false"
+                            data-select2-id="select2-site-area-result-mv91-minnesota">
+                            <span>Minnesota</span><span class="min-price">Min: $120</span></li>
+                        <li class="select2-results__option" id="select2-site-area-result-lv5u-new-york"
+                            role="option" aria-selected="false"
+                            data-select2-id="select2-site-area-result-lv5u-new-york"><span>New York</span><span
+                                class="min-price">Min: $110</span></li>
+                        <li class="select2-results__option" id="select2-site-area-result-t7xe-washington"
+                            role="option" aria-selected="false"
+                            data-select2-id="select2-site-area-result-t7xe-washington">
+                            <span>Washington</span><span class="min-price">Min: $130</span></li>
+                    </ul>
+                </span></span></span></div><!-- search-location -->
+</div><!-- select-location-wrapper -->
+<div class="location-overlay"></div>
+</div>`
+
+let modal = document.querySelector(".modal");
+
+
+document.querySelector(".btn").addEventListener("click", function(){
+    let modalBody = document.querySelector(".modal-body");
+
+    modalBody.innerHTML = modalContent;
+})
