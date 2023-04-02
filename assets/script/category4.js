597eabd9f144cd054fae7aedf931a5ee12057bfc
kamranss <kamranss@code.edu.az>
2023-04-02 20:16:37 +0400

    Refactor: Category4 Slider partially fixed

--- assets/script/category4.js
+++ assets/script/category4.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-    $('.category2-products').slick({
+    $('.category2-right-side').slick({
         centerMode: true,
         centerPadding: '60px',
-        slidesToShow: 3,
+        slidesToShow: 5,
         responsive: [
           {
             breakpoint: 768,
@@ -28,25 +28,47 @@ $(document).ready(function(){
 
 
       let buttonPrev = `
-      <i class="fa-solid fa-chevron-left bs-prev "></i>
+      <div class="slider-arrow-box-prev d-flex align-items-center justify-content-center">
+        <i class="fa-solid fa-chevron-left bs-prev "></i>
+      </div>
+     
      `
 
       let buttonNext = `
-      <i class="fa-solid fa-chevron-right bs-next"></i>
+      <div class="slider-arrow-box-next">
+        <i class="fa-solid fa-chevron-right bs-next"></i>
+      </div>
       `
 
 
-    //  let category2 =  $(".category2-products");
+     let category2 =  $(".category2-right-side");
       
-      // let newButtonPrev = $(".carousel-control-prev");
-      // let newButtonNext = $(".carousel-control-next");
-
       let sliderButtonPrev = $(".slick-prev");
-      sliderButtonPrev.text("");
-      let sliderButtonNext = $(".slick-next");
+      let sliderButtonNext = $(".slick-next"); 
+       // sliderButtonPrev.text("");
 
-      sliderButtonPrev.append(buttonPrev);
-      sliderButtonNext.append(buttonNext); 
+       category2.append(buttonPrev);
+       category2.append(buttonNext); 
+
+      let newButtonPrev = $(".fa-chevron-left");
+      let newButtonNext = $(".fa-chevron-right");
+       
+      newButtonPrev.on('click', function() {
+        // Trigger a click event on the slider button
+        sliderButtonPrev.trigger('click');
+      });
+      
+      newButtonNext.on('click', function() {
+        // Trigger a click event on the slider button
+        sliderButtonNext.trigger('click');
+      });
+
+
+      
+
+
+      // sliderButtonPrev.append(buttonPrev);
+      // sliderButtonNext.append(buttonNext);
 
 
 
