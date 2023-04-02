0c3091c731391ca1c5f5c27abb1c325f184f292b
kamranss <kamranss@code.edu.az>
2023-04-02 17:29:25 +0400

    Refactor: Task updated

--- assets/script/category4.js
+++ assets/script/category4.js
@@ -24,4 +24,31 @@ $(document).ready(function(){
           }
         ]
       });
+
+
+
+      let buttonPrev = `
+      <i class="fa-solid fa-chevron-left bs-prev "></i>
+     `
+
+      let buttonNext = `
+      <i class="fa-solid fa-chevron-right bs-next"></i>
+      `
+
+
+    //  let category2 =  $(".category2-products");
+      
+      // let newButtonPrev = $(".carousel-control-prev");
+      // let newButtonNext = $(".carousel-control-next");
+
+      let sliderButtonPrev = $(".slick-prev");
+      sliderButtonPrev.text("");
+      let sliderButtonNext = $(".slick-next");
+
+      sliderButtonPrev.append(buttonPrev);
+      sliderButtonNext.append(buttonNext); 
+
+
+
 })
+
